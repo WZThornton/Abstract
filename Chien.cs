7bf4f69a5b96eeb1fb602def087d6c15bbee5a1a
william thornton <WZThornton@hotmail.com>
2022-04-26 15:59:08 -0400

    Add project files.

---
+++ Chien.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Chien : Animal
+    {
+        public override void gender()
+        {
+            male = false;
+        }
+    }
+}
